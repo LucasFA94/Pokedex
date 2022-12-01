f9576dddc393249843dc936099c2890490d7fbee
LucasFA94 <lucasfernandesafonso@gmail.com>
2022-11-30 21:14:18 -0300

    destructuring the pokeApi.js

--- services/pokeApi.js
+++ services/pokeApi.js
@@ -38,24 +38,24 @@ class PokeApi{
             this.getPokemon(id), 
             this.getPokemonSpecie(id)
         ])
-        .then(axios.spread((pokemon,pokemonSpecie)=>(
+        .then(axios.spread(({name, types, height, weight, abilities},{names, pokedex_numbers, gender_rate, genera, capture_rate, hatch_counter})=>(
             {
             pictures:{
                 oficial: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`
             },
-            name: this.#makingName(pokemon.name),
-            nameList: this.#allNames(pokemonSpecie.names),
-            no: pokemonSpecie.pokedex_numbers[0].entry_number,
-            gender: pokemonSpecie.gender_rate<0? "genderless": {male: 1, female: 2},
-            types: pokemon.types.map((type)=>{
-                return this.#makingName(type.type.name);
+            name: this.#makingName(name),
+            nameList: this.#allNames(names),
+            no: pokedex_numbers[0].entry_number,
+            gender: gender_rate<0? "genderless": {male: 1, female: 2},
+            types: types.map(({type:{name}})=>{
+                return this.#makingName(name);
             }),
-            classification: this.#getGenus(pokemonSpecie.genera),
-            height: this.#getHeight(pokemon.height),
-            weight: this.#getWeight(pokemon.weight),
-            captureRate: pokemonSpecie.capture_rate,
-            hatch: this.#getHatchCounter(pokemonSpecie.hatch_counter),
-            abilities: getAbilities(pokemon.abilities)
+            classification: this.#getGenus(genera),
+            height: this.#getHeight(height),
+            weight: this.#getWeight(weight),
+            captureRate: capture_rate,
+            hatch: this.#getHatchCounter(hatch_counter),
+            abilities: getAbilities(abilities)
         })))
         .catch((err)=>{
             console.err(err.response.data);
@@ -70,12 +70,12 @@ class PokeApi{
 
     //Get names from other language
     static #allNames = (list)=>{
-        let result = list.map((lang)=>{
-            let name = lang.name
-            let lName = lang.language.name
-            let language = ISO6391.getName(lName);
-            return {language:language, name:name};
-        }).filter((lang)=> lang.language !== "")
+        let result = list.map(({name,language})=>{
+            let lname = name;
+            let cName = language.name;
+            let cLanguage = ISO6391.getName(cName);
+            return {language:cLanguage, name:lname};
+        }).filter(({language})=> language !== "")
 
         return result;
 
@@ -83,7 +83,7 @@ class PokeApi{
 
     //Get Classification in en
     static #getGenus = (values)=>{
-        const result = values.find(value => value.language.name == "en");
+        const result = values.find(({language:{name}}) => name == "en");
         return result.genus;
     }
 
